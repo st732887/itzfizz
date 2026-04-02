50edd4714e793411b1b2b9983e8faf280ea6075b
st732887 <161117249+st732887@users.noreply.github.com>
2026-04-03 00:12:35 +0530

    Create page.js

---
+++ keyframes/page.js
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar'
+import HeroSection from '../components/HeroSection'
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <HeroSection />
+      </main>
+    </>
+  )
+}
